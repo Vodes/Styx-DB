acd8b3de00a05528b5d6c688e848b8b6dd9edbc7
Alex H <48185123+Vodes@users.noreply.github.com>
2024-01-12 22:11:02 +0100

    More legacy stuff

--- lib/src/main/kotlin/moe/styx/db/LegacyExtensions.kt
+++ lib/src/main/kotlin/moe/styx/db/LegacyExtensions.kt
@@ -2,10 +2,12 @@ package moe.styx.db
 
 import moe.styx.types.LegacyAnimeInfo
 import moe.styx.types.LegacyEpisodeInfo
+import moe.styx.types.LegacyMovieInfo
 import moe.styx.types.toBoolean
+import java.net.URLDecoder
 import java.net.URLEncoder
 
-fun StyxDBClient.save(episodeInfo: LegacyEpisodeInfo, newID: String?): Boolean {
+fun StyxDBClient.save(episodeInfo: LegacyEpisodeInfo, newID: String? = null): Boolean {
     val edit = objectExists(episodeInfo.GUID, "Episodes", "PermID")
     val query = if (edit)
         "UPDATE Episodes SET GUID=?, PermID=?, Date=?, Name=?, EP=?, ep_name_en=?, ep_name_de=?, summary_en=?, summary_de=?, FilePath=?, FileSize=?, PrevName=? WHERE PermID=?;"
@@ -34,7 +36,7 @@ fun StyxDBClient.save(episodeInfo: LegacyEpisodeInfo, newID: String?): Boolean {
 
 fun StyxDBClient.getLegacyEpisodes(conditions: Map<String, Any>? = null): List<LegacyEpisodeInfo> {
     val episodes = mutableListOf<LegacyEpisodeInfo>()
-    openResultSet("SELECT * FROM Episodes;", conditions) {
+    openResultSet("SELECT * FROM Episodes ORDER BY EP;", conditions) {
         while (next())
             episodes.add(
                 LegacyEpisodeInfo(
@@ -60,7 +62,7 @@ fun StyxDBClient.delete(episodeInfo: LegacyEpisodeInfo) = genericDelete(episodeI
 
 // LegacyAnimeInfo
 
-fun StyxDBClient.save(animeInfo: LegacyAnimeInfo, newID: String?): Boolean {
+fun StyxDBClient.save(animeInfo: LegacyAnimeInfo, newID: String? = null): Boolean {
     val edit = objectExists(animeInfo.GUID, "Anime")
     val query = if (edit)
         "UPDATE Anime SET GUID=?, Name=?, SeasonName=?, ListLink=?, CoverURL=?, CoverMD5=?, English=?, Romaji=?, Schedule=?, Synopsis=?, DbMapping=? WHERE GUID=?;"
@@ -77,7 +79,7 @@ fun StyxDBClient.save(animeInfo: LegacyAnimeInfo, newID: String?): Boolean {
         setString(7, animeInfo.English)
         setString(8, animeInfo.Romaji)
         setString(9, animeInfo.Schedule)
-        setString(10, URLEncoder.encode(animeInfo.Synopsis, "UTF-8"))
+        setString(10, URLEncoder.encode(animeInfo.Synopsis, Charsets.UTF_8))
         setString(11, animeInfo.DbMapping)
 
         if (edit)
@@ -101,7 +103,7 @@ fun StyxDBClient.getLegacyAnime(conditions: Map<String, Any>? = null): List<Lega
                     getString("English"),
                     getString("Romaji"),
                     getString("Schedule"),
-                    getString("Synopsis"),
+                    URLDecoder.decode(getString("Synopsis"), Charsets.UTF_8),
                     null,
                     getString("DbMapping")
                 )
@@ -111,3 +113,59 @@ fun StyxDBClient.getLegacyAnime(conditions: Map<String, Any>? = null): List<Lega
 }
 
 fun StyxDBClient.delete(animeInfo: LegacyAnimeInfo) = genericDelete(animeInfo.GUID, "Anime")
+
+// LegacyMovieInfo
+
+fun StyxDBClient.save(movieInfo: LegacyMovieInfo, newID: String? = null): Boolean {
+    val edit = objectExists(movieInfo.permID, "Movies", "permID")
+    val query = if (edit)
+        "UPDATE Movies SET GUID=?, PermID=?, Date=?, Name=?, ListLink=?, CoverURL=?, CoverMD5=?, English=?, Romaji=?, Synopsis=?, FilePath=?, FileSize=?, Category=? WHERE PermID=?;"
+    else
+        "INSERT INTO Movies (GUID, PermID, Date, Name, ListLink, CoverURL, CoverMD5, English, Romaji, Synopsis, FilePath, FileSize, Category) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
+
+    val stat = openStatement(query) {
+        setString(1, if (newID.isNullOrBlank()) movieInfo.permID else newID)
+        setString(2, if (newID.isNullOrBlank()) movieInfo.permID else newID)
+        setString(3, movieInfo.date)
+        setString(4, movieInfo.name)
+        setString(5, movieInfo.listLink)
+        setString(6, movieInfo.coverURL)
+        setString(7, movieInfo.coverMD5)
+        setString(8, movieInfo.English)
+        setString(9, movieInfo.Romaji)
+        setString(10, URLEncoder.encode(movieInfo.synopsis, Charsets.UTF_8))
+        setString(11, movieInfo.filePath)
+        setDouble(12, movieInfo.fileSize)
+        setString(13, movieInfo.category)
+        if (edit)
+            setString(14, movieInfo.permID)
+
+    }
+    return stat.executeUpdate().toBoolean().also { stat.close() }
+}
+
+fun StyxDBClient.getLegacyMovieList(conditions: Map<String, Any>? = null): List<LegacyMovieInfo> {
+    val movies = mutableListOf<LegacyMovieInfo>()
+    openResultSet("SELECT * FROM Movies;", conditions) {
+        while (next())
+            movies.add(
+                LegacyMovieInfo(
+                    getString("PermID"),
+                    getString("Date"),
+                    getString("Name"),
+                    getString("ListLink"),
+                    getString("CoverURL"),
+                    getString("CoverMD5"),
+                    getString("English"),
+                    getString("Romaji"),
+                    URLDecoder.decode(getString("Synopsis"), Charsets.UTF_8),
+                    getString("FilePath"),
+                    getDouble("FileSize"),
+                    getString("Category")
+                )
+            )
+    }
+    return movies.toList()
+}
+
+fun StyxDBClient.delete(movieInfo: LegacyMovieInfo) = genericDelete(movieInfo.permID, "Movies", "PermID")
