c9a071b0d8ffc25ee346c4317599149930ca84d9
Alex H <48185123+Vodes@users.noreply.github.com>
2024-01-28 14:22:14 +0100

    Add cursed task to build types library from git

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,8 +1,11 @@
+import org.ajoberstar.grgit.Grgit
+
 plugins {
     alias(libs.plugins.jvm)
     alias(libs.plugins.serialization)
     `java-library`
     `maven-publish`
+    id("org.ajoberstar.grgit") version ("5.2.1")
 }
 
 group = "moe.styx"
@@ -36,3 +39,28 @@ publishing {
         }
     }
 }
+
+tasks.register("buildExternalDeps") {
+    val isWin = System.getProperty("os.name").contains("win", true)
+    val projectDir = layout.projectDirectory.asFile.parentFile
+    val outDir = File(projectDir, ".temp-deps/styx-types")
+    doFirst {
+        outDir.deleteRecursively()
+        Grgit.clone {
+            dir = outDir
+            uri = "https://github.com/Vodes/Styx-Types.git"
+        }
+        val result = kotlin.runCatching {
+            ProcessBuilder(listOf(if (isWin) "./gradlew.bat" else "./gradlew", "publishToMavenLocal"))
+                .directory(outDir)
+                .inheritIO()
+                .start().waitFor()
+        }.getOrNull() ?: -1
+        if (result != 0) {
+            outDir.deleteRecursively()
+            throw StopExecutionException()
+        }
+    }
+    doLast { outDir.deleteRecursively() }
+}
+
